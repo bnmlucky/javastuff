6a7a7ed22f2f4f791850a9c2f01f2e1d367f095e
Natalia Titova <bnmlucky@gmail.com>
2021-10-18 14:40:06 -0700

    gave the instance an argument

--- src/com/company/Store.java
+++ src/com/company/Store.java
@@ -4,15 +4,17 @@ public class Store {
         // declare instance fields here
         String productType;
         // new method: constructor!
-        public Store() {
+        public Store(String product) {
             System.out.println("I am inside the constructor method.");
+            productType = product;
         }
         // main method is where we create instances
         public static void main(String[] args) {
             System.out.println("Start of the main method.");
             // create the instance below
-            Store lemonadeStand = new Store();
+            Store lemonadeStand = new Store("lemonade");
             // print the instance below
             System.out.println(lemonadeStand);
+            System.out.println(lemonadeStand.productType);
         }
 }
