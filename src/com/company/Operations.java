693c80a0bff9118d9044344ac6b61729505c6cc8
Natalia Titova <bnmlucky@gmail.com>
2021-10-18 10:05:49 -0700

    another expression for order of operations

--- src/com/company/Operations.java
+++ src/com/company/Operations.java
@@ -4,5 +4,8 @@ public class Operations {
     public static void main(String[] args) {
         int expression1 = 5 % 2 - (4 * 2 - 1);
         System.out.println(expression1);
+
+        int expression2 = (3 + (2 * 2 - 5)) + 6 - 5;
+        System.out.println(expression2);
     }
 }
