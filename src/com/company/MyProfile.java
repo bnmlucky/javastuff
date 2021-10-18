f9748d9ca007e2b61ac2149589a2df2ff180f8e0
Natalia Titova <bnmlucky@gmail.com>
2021-10-18 09:10:55 -0700

    more Java classes where I tried working with different types of variables

---
+++ src/com/company/MyProfile.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class MyProfile {
+    public static void main(String[] args) {
+        String name = "Natalia Titova";
+        int age = 41;
+        double desiredSalary = 140000.00;
+        char gender = 'f';
+        boolean lookingForJob = false;
+    }
+}
