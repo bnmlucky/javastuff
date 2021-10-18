6d8d03cfda36cd557c4d23d1bc041967f703ee64
Natalia Titova <bnmlucky@gmail.com>
2021-10-18 12:07:23 -0700

    final keyword - can't reassign anything

---
+++ src/com/company/Final.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Final {
+    public static void main(String[] args) {
+        final double pi = 3.14;
+        System.out.println(pi);
+        // pi ++; java: cannot assign a value to final variable pi
+    }
+}
