8a16abbbc331740bba5fbada3481e677e53ab71a
Natalia Titova <bnmlucky@gmail.com>
2021-10-18 10:03:52 -0700

    order of operations

---
+++ src/com/company/Operations.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Operations {
+    public static void main(String[] args) {
+        int expression1 = 5 % 2 - (4 * 2 - 1);
+        System.out.println(expression1);
+    }
+}
