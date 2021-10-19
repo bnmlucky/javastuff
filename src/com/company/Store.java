d6368fae473c5b5df2b3924d9345a0a2d8bfe972
Natalia Titova <bnmlucky@gmail.com>
2021-10-18 17:41:57 -0700

    created more parameters and passed them as arguments to a new instance

--- src/com/company/Store.java
+++ src/com/company/Store.java
@@ -3,18 +3,23 @@ package com.company;
 public class Store {
         // declare instance fields here
         String productType;
+        int inventoryCount;
+        double inventoryPrice;
         // new method: constructor!
-        public Store(String product) {
+        public Store(String product, int count, double price) {
             System.out.println("I am inside the constructor method.");
             productType = product;
+            inventoryCount = count;
+            inventoryPrice = price;
         }
         // main method is where we create instances
         public static void main(String[] args) {
             System.out.println("Start of the main method.");
             // create the instance below
-            Store lemonadeStand = new Store("lemonade");
+            //Store lemonadeStand = new Store("lemonade");
             // print the instance below
-            System.out.println(lemonadeStand);
-            System.out.println(lemonadeStand.productType);
+            //System.out.println(lemonadeStand);
+            //System.out.println(lemonadeStand.productType);
+            Store cookieShop = new Store("cookies", 12, 3.75);
         }
 }
